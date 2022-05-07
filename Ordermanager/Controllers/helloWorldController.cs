6cdf7981f254c48e2fe0617eaeb6ec77b278850c
hope-you <595318443@qq.com>
2022-05-07 16:38:44 +0800

    添加项目文件。

---
+++ Ordermanager/Controllers/helloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Ordermanager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class helloWorldController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public helloWorldController(ILogger<helloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string helloWorld()
+        {
+            return "Hello World!";
+        }
+    }
+}
